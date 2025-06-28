7300c5e46a048672c5a16ea7256a74878e89b118
lastminutky <m.skircak@gmail.com>
2025-06-28 10:46:23 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# lastminutky-image-scraper
-Scraper for hotel images from lastminutky.sk
+# Lastminutky Hotel Image Scraper
+
+This Apify actor searches for a hotel on www.lastminutky.sk and extracts up to 4 image URLs from the hotel's detail page.
+
+## Input
+
+```json
+{
+  "hotelName": "Cavomarina Beach"
+}
+```
+
+## Output
+
+```json
+{
+  "images": [
+    "https://www.lastminutky.sk/images/hotel1.jpg",
+    ...
+  ]
+}
+```
---
+++ main.js
@@ -0,0 +1,36 @@
+const Apify = require('apify');
+
+Apify.main(async () => {
+    const input = await Apify.getInput();
+    const hotelName = input.hotelName;
+    const searchUrl = \`https://www.lastminutky.sk/vyhladavanie-zajazdov/1/?q=\${encodeURIComponent(hotelName)}\`;
+
+    const requestQueue = await Apify.openRequestQueue();
+    await requestQueue.addRequest({ url: searchUrl, userData: { type: 'search' } });
+
+    const imageUrls = [];
+
+    const crawler = new Apify.CheerioCrawler({
+        requestQueue,
+        handlePageFunction: async ({ request, $ }) => {
+            if (request.userData.type === 'search') {
+                const hotelLink = $('a.card-title').attr('href');
+                if (hotelLink) {
+                    const fullUrl = \`https://www.lastminutky.sk\${hotelLink}\`;
+                    await requestQueue.addRequest({ url: fullUrl, userData: { type: 'detail' } });
+                }
+            } else if (request.userData.type === 'detail') {
+                $('img').each((index, el) => {
+                    const src = $(el).attr('data-src') || $(el).attr('src');
+                    if (src && imageUrls.length < 4 && src.match(/^https?:\/\/.*\.(jpg|jpeg|png)$/)) {
+                        imageUrls.push(src);
+                    }
+                });
+            }
+        },
+        maxRequestsPerCrawl: 10,
+    });
+
+    await crawler.run();
+    await Apify.setValue('OUTPUT', { images: imageUrls });
+});
